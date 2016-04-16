fb549082a851a2cac8da8efe448592d6d9b7bd88
fhsgoncalves <fernando.hsgoncalves@gmail.com>
2016-04-15 21:54:58 -0300

    finish job

--- build.sbt
+++ build.sbt
@@ -4,9 +4,11 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
+resolvers += Resolver.sonatypeRepo("releases")
+
 libraryDependencies ++= Seq(
   "org.apache.spark" % "spark-core_2.11" % "1.6.1"
   , "org.apache.spark" % "spark-mllib_2.11" % "1.6.1"
-  //, "com.github.melrief" % "purecsv" % "0.0.6"
+  , "com.github.melrief" % "purecsv_2.11" % "0.0.6"
 //  , compilerPlugin("org.scalamacros" % "paradise" % "2.0.1" cross CrossVersion.full)
 )
--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -3,28 +3,28 @@
 import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.{SparkConf, SparkContext}
+import purecsv.unsafe._
 
 object App {
 
-  case class TipoDeCrime(tipo: String)
+  case class CsvLine(c1: String, tipo: String, c3: String, c4: String, c5: String, c6: String, c7: String, lon: Double, lat: Double)
 
   def main(args: Array[String]) {
-    val logFile = "test.txt" // Should be some file on your system
+    val fileCsv = "kmeans_1.csv" // Should be some file on your system
     val conf = new SparkConf().setMaster("local[*]").setAppName("MC855 - Spark Jobs")
     val sc = new SparkContext(conf)
-    val file = sc.textFile(logFile, 2).cache()
+    val file = sc.textFile(fileCsv, 2).cache()
+    val header = file.first() // extract header
 
-    val tiposDeCrime = file.map(t => {
-      val parts = t.split(",")
-      TipoDeCrime(parts(1))
-    })
+    val lines = file.filter(x => x != header).map(t => CSVReader[CsvLine].readCSVFromString(t).head).cache()
+    val tiposDeCrime = lines.map(t => t.tipo)
       .distinct()
       .collect()
 
     var i = 0
     var tiposDeCrimeMap: Map[String, Int] = Map()
     tiposDeCrime.foreach(t => {
-      tiposDeCrimeMap += (t.tipo -> i)
+      tiposDeCrimeMap += (t -> i)
       i += 1
     })
 
@@ -32,17 +32,16 @@ object App {
 
     val quantidadeDeCrimes = tiposDeCrimeMap.size
 
-    val parsedData = file.map(s => {
-      val parts = s.split(",")
-      val tipoDeCrime = parts(1)
+    val parsedData = lines.map(s => {
+      val tipoDeCrime = s.tipo
 
       var vetor = Array.fill[Double](quantidadeDeCrimes)(0D)
 
       val vetorIndex = tiposDeCrimeMap(tipoDeCrime)
       vetor(vetorIndex) = 1
 
-      vetor :+= parts(8).toDouble
-      vetor :+= parts(7).toDouble
+      vetor :+= s.lat
+      vetor :+= s.lon
 
       Vectors.dense(vetor)
     }).cache()
@@ -51,10 +50,11 @@ object App {
     val numInterations = 100
     val clusters = KMeans.train(parsedData, numClusters, numInterations)
 
-    val WSSSE = clusters.computeCost(parsedData)
-    println(s"Within set sum of squared errors = $WSSSE")
-    clusters.save(sc, "output")
-    val sameModel = KMeansModel.load(sc, "output")
-    println("finish")
+//    val WSSSE = clusters.computeCost(parsedData)
+//    println(s"Within set sum of squared errors = $WSSSE")
+//    clusters.save(sc, "output")
+//    val sameModel = KMeansModel.load(sc, "output")
+
+    println(s"Result = ${clusters.clusterCenters}")
   }
 }
